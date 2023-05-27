49225b4b172015fbf2f99ec4c9503be81fcf8fae
berol24 <124741895+berol24@users.noreply.github.com>
2023-05-27 11:07:44 +0200

    Add files via upload
    j'ai fait la page de profil qui permettra à l'utilisateur de modifier son profil

---
+++ profil.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="profil.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com%22%3E/
+<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="profil">
+            <!-- <header class="logo">
+                <img src="./img/logo-socialconnect.png" alt="profil"> 
+                <button>Se Deconnecter</button>
+            </header> -->
+            <div class="contain">
+                    <div class="contain-left">
+                    <img src="./img/logo-socialconnect.png" alt="profil"> 
+                            <ul>
+                                <li>
+                                    <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                                    <span class="text">Acceuil</span>
+                                </li>
+                                <li>
+                                    <span class="icon"><ion-icon name="search-outline"></ion-icon></span>
+                                    <span class="text">Recherche</span>
+                                </li>
+                                <li>
+                                    <span class="icon"><ion-icon name="chatbubble-outline"></ion-icon></span>
+                                    <span class="text">Message</span>
+                                </li>
+                                <li>
+                                    <span class="icon"><ion-icon name="add-circle-outline"></ion-icon></i></span>
+                                    <span class="text">Créer</span>
+                                </li>
+                                <li>
+                                    <span class="icon"><ion-icon name="heart-outline"></ion-icon></span>
+                                    <span class="text">Notifications</span>
+                                </li>
+                                <li>
+                                    <span class="icon"><ion-icon name="person-circle-outline"></ion-icon></span>
+                                    <span class="text">Profil</span>
+                                </li>
+                                <li>
+                                    <span class="icon"><ion-icon name="menu-outline"></ion-icon></span>
+                                    <span class="text">Plus</span>
+                                </li>
+                            </ul>
+                    </div>
+
+
+
+                <div class="contain-right">
+                        <img src="./img/picture.png" alt="user-profil" > 
+                        <ion-icon class="add-profil" name="add-circle-outline"></ion-icon>
+                        <div class="name-user">
+                            <h2>Faberol</h2> 
+                            <button>Modifier Profil</button>
+                            <button>Se Deconnecter</button>
+                        </div>
+                        <div class="info">
+                            <div class="abonnement">
+                                <article>Abonnés : <span>0</span></article>
+                                <article>Abonnements :<span>0</span></article> 
+                                <article>Abonnements :<span>0</span></article> 
+                            </div>                       
+                            <input type="text"> 
+                        </div>
+                        <div class="other-profil">
+                            <img style="width: 150px;" src="./img/API.png" alt="image1">
+                            <img style="width: 125px;" src="./img/tour effeil-jour.jpeg" alt="image2">
+                            <ion-icon name="add-circle-outline"></ion-icon>
+                        </div>
+
+                        <hr>
+
+                        <div class="photo">
+                            <a href="#"><ion-icon name="camera-outline"></ion-icon></a>
+                            <h2>Partager des photos</h2>
+                           <p>Lorsque vous partager des photos , elles apparaissent sur votre profil.</p>
+                           <span>Partager votre première photo</span> 
+                        </div>
+                </div>
+           
+            </div>
+
+
+
+    </div>
+
+
+
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
